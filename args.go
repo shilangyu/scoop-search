692284929dc50794df6ab232810fd9047ea7efaf
shilangyu <xmarcinmarcin@gmail.com>
2020-07-26 12:08:36 +0200

    removed `all` from parsed args, not being used

--- args.go
+++ args.go
@@ -5,22 +5,21 @@ import "os"
 type parsedArgs struct {
 	query string
 	hook  bool
-	all   bool
 }
 
 func parseArgs() parsedArgs {
-	var all, hook bool
+	var hook bool
 	var query string
 
 	if len(os.Args) == 1 {
-		all = true
+		// pass
 	} else if os.Args[1] == "--hook" {
 		hook = true
 	} else {
 		query = os.Args[1]
 	}
 
-	return parsedArgs{query, hook, all}
+	return parsedArgs{query, hook}
 }
 
 const poshHook = `function scoop { if ($args[0] -eq "search") { scoop-search.exe @($args | Select-Object -Skip 1) } else { scoop.ps1 @args } }`
