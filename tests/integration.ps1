8fe784b15fb38900fca5e1c6beecbeb13a1cf5a7
shilangyu <xmarcinmarcin@gmail.com>
2020-07-26 12:04:52 +0200

    fix lint issues

--- tests/integration.ps1
+++ tests/integration.ps1
@@ -1,15 +1,16 @@
 go build .
 
-$terms = "fl", "android", "sudo", "", "azure", "t", "a"
+$terms = "fl", "android", "sudo", "", "azure", "t", "a", "P", "G", "as"
 
 foreach ($term in $terms) {
 	$s1 = (./scoop-search.exe $term)
 	$s2 = scoop.ps1 search $term *>&1
 
 	if ([string]$s1 -ne [string]$s2) {
-		echo "term '$term' failed: "
+		Write-Output "term '$term' failed:"
 		$s1 > .\tests\got.out
 		$s2 > .\tests\expected.out
 		git diff --no-index .\tests\got.out .\tests\expected.out
+		Write-Output ""
 	}
 }
